e40b1482c8cbe0ef369415567900b6ad63369c64
Mañana <Maana@Aula8-03>
2024-04-16 09:43:36 +0200

    SOLUCION EJERCICIO 1 CORREGIDO POR MARCOS

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -2,23 +2,24 @@ package ejercicio1;
 
 public class Calculadora {
 	
-	public int sumar(int num1, int num2) {
-        return num1+num2;
+	public int sumar(int sumando1, int sumando2) {
+        return sumando1+sumando2;
     }
 
-    public int restar(int num1, int num2) {
-        return num1-num2;
+    public int restar(int minuendo, int sustraendo) {
+        return minuendo-sustraendo;
     }
 
-    public int multiplicar(int num1, int num2) {
-        return num1*num2;
+    public int multiplicar(int factor1, int factor2) {
+        return factor1*factor2;
     }
 
-    public float dividir(float num1, float num2) {
-        if (num2 == 0) {
-            return 0;
+    public float dividir(float dividendo, float divisor) {
+        if (divisor == 0) {
+            //TODO DIVISION POR CERO. ¿LANZAR EXCEPCION ?
+        	return 0;
         } else {
-            return num1/num2;
+            return dividendo/divisor;
         }
     }
 

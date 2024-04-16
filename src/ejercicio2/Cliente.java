bf029dbee9c55a94d45e4a5b6b82f05017d7c63a
Mañana <Maana@Aula8-03>
2024-04-16 10:18:21 +0200

    SOLUCION HECHA POR MARCOS

--- src/ejercicio2/Cliente.java
+++ src/ejercicio2/Cliente.java
@@ -2,12 +2,6 @@ package ejercicio2;
 
 public class Cliente {
 	
-	  public void agregarCliente(Cliente cliente) {
-	        // Lógica para agregar un cliente
-	    }
-	  
-	  public void eliminarCliente(Cliente cliente) {
-	        // Lógica para eliminar un cliente
-	    }
+	 
 	
 }
--- src/ejercicio2/Tienda.java
+++ src/ejercicio2/Tienda.java
@@ -3,12 +3,31 @@ package ejercicio2;
 import java.util.List;
 
 public class Tienda {
-	
-	    private List<Producto> productos;
-	    private List<Cliente> clientes;
-	    private List<Pedido> pedidos;
 
-	    // Otros métodos relacionados con la gestión de la tienda
+	private List<Producto> productos;
+	private List<Cliente> clientes;
+	private List<Pedido> pedidos;
 	
+	public void realizarPedido(Cliente cliente, List<Producto> productos) {
+		// Lógica para realizar un pedido
+	}
+
+	public void agregarProducto(Producto producto) {
+		// Lógica para agregar un producto
+	}
+
+	public void eliminarProducto(Producto producto) {
+		// Lógica para eliminar un producto
+	}
+
+	public void agregarCliente(Cliente cliente) {
+		// Lógica para agregar un cliente
+	}
+
+	public void eliminarCliente(Cliente cliente) {
+		// Lógica para eliminar un cliente
+	}
+
+	// Otros métodos relacionados con la gestión de la tienda
 
 }

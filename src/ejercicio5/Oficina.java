428e97a4a545ade20cecbce7795bd2838b194256
Mañana <Maana@Aula8-03>
2024-04-30 09:07:06 +0200

    INTENTO DE SOLUCION POR MI SHOTGUN SURGERY

--- src/ejercicio5/Oficina.java
+++ src/ejercicio5/Oficina.java
@@ -1,11 +1,14 @@
 package ejercicio5;
 
-public class OtraClaseMas {
+public class Oficina {
 	
+    public void setDireccion(Cliente cliente, String nuevaDireccion) {
+        cliente.setDireccion(nuevaDireccion);
+        // Otro código aquí
+    }
+
     public void enviarCorreo(Cliente cliente) {
         String direccion = cliente.getDireccion();
         // Lógica para enviar correo a la dirección
     }
-
-	
 }
